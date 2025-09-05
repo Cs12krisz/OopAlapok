bb953876ebd2ce90fe6bf872abb4403578a6d917
Cs12krisz <csigerkrisztian@gmail.com>
2025-09-05 11:25:24 +0200

    Feladat1 is ready.

--- OopAlapok/Program.cs
+++ OopAlapok/Program.cs
@@ -6,12 +6,22 @@ using System.Threading.Tasks;
 
 namespace OopAlapok
 {
+    class Szemely
+    {
+        public string nev = "Név";
+        public int kor = 35;
+
+    }
+
     internal class Program
     {
+        
+
         static void Main(string[] args)
         {
-
-
+            Szemely tanulo = new Szemely();
+            Console.WriteLine(tanulo.nev);
+            Console.WriteLine(tanulo.kor);
         }
     }
 }
